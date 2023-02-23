16bacfaa9585b65f0752480ca6e4bb33c6a4d1ec
Nurbolat Abdraim <nurlight05@gmail.com>
2023-02-23 12:15:54 +0600

    отрисовка страницы логина

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-main>
+      <component :is="layout">
+        <router-view/>
+      </component>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import AuthLayout from '@/layouts/AuthLayout.vue'
+export default {
+  
+
+  computed: {
+    layout(){
+      return (this.$route.meta.layout || 'auth') + '-layout'
+    }
+  },
+  components: {
+      AuthLayout
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-layout class="layout">
+        <div class="auth" >
+            <v-container class="container">
+                <div class="auth__inner  d-flex justify-center">
+                    <v-col cols="6">
+                        <v-card class="auth__card">
+                            <router-view/>
+                        </v-card>
+                    </v-col>
+                </div>
+            </v-container>
+        </div>
+    </v-layout>
+</template>
+
+
+<style lang="scss" scoped>
+
+.auth{
+    width: 100%;
+    &__inner{
+        
+    }
+    &__card{
+        padding: 30px;
+    }
+}
+.container{
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+.layout{
+    min-height: 100vh;
+    
+}
+</style>
