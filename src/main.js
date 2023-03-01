f2dd244da3d5d947665744a2d3fd97f06981d553
Nurbolat Abdraim <nurlight05@gmail.com>
2023-03-01 14:05:19 +0600

    Реализация страницы логина без валидации

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+
 import { loadFonts } from './plugins/webfontloader'
 import './assets/styles/main.scss';
+
+
 
 loadFonts()
 
